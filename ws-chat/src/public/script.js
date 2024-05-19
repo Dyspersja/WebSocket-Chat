874ec2018793fe3b9bff368ed93fd460736a2514
Dyspersja <dyspersjamusic@gmail.com>
2024-05-19 23:52:05 +0200

    Added login functionality to client side

--- ws-chat/src/public/script.js
+++ ws-chat/src/public/script.js
@@ -15,9 +15,41 @@ $(document).ready(function() {
         
         ws.onopen = function() {
             console.log('Connected to websocket server'); 
+            ws.send(JSON.stringify({ 
+                type: 'login', 
+                username: username 
+            }));
         };
-        
-        $('#loginModal').hide();
+
+        ws.onmessage = function(event) {
+            let data = JSON.parse(event.data);
+
+            switch(data.type) {
+                case 'success':
+                    if (data.message === 'Logged in successfully') {
+                        $('#loginModal').hide();
+                        $('#currentUser').text('Logged in as: ' + username);
+                    }
+                    break;
+                case 'error':
+                    if (data.message === 'Username already taken') {
+                        alert('Username already taken');
+                    }
+                    break;
+                case 'userList':
+                    // TODO: Finish
+                    break;
+                case 'userLogin':
+                    // TODO: Finish
+                    break;
+                case 'userLogout':
+                    // TODO: Finish
+                    break;
+                case 'message':
+                    // TODO: Finish
+                    break;
+            }
+        }
     });
 
     $('#messageForm').submit(function(event) {
